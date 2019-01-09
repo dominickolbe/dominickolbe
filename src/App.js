f56a0ef45c9ee5feba73b243d188dca2ee43ee21
Dominic Kolbe <kolbedominic@gmail.com>
2019-01-09 01:47:15 +0100

    create simple design and text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const HeroHeadline = styled.h1`
+  font-size: 3em;
+  font-weight: 700;
+  max-width: 750px;
+`;
+const HeroText = styled.p`
+  font-size: 1.5em;
+  font-weight: 500;
+  max-width: 750px;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>dominickolbe</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <HeroHeadline>
+              Hey there, my name is Dominic, developer, I love music and video games.
+            </HeroHeadline>
+            <HeroText>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </HeroText>
+            <a href="mailto:hi@dominickolbe.dk">hi@dominickolbe.dk</a>
+          </div>
+        </div>
       </div>
     );
   }
