d019d5e57c2b6aa6b675448dfd16c33e816692e3
Dominic Kolbe <kolbedominic@gmail.com>
2019-01-12 21:46:42 +0100

    refactor: update content

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import data from './data.json';
+import content from './content.json';
+
+const Container = styled.div`
+  padding: 50px;
+`;
 
 const HeroHeadline = styled.h1`
   font-size: 3em;
   font-weight: 700;
   letter-spacing: .03em;
   line-height: 1.2;
+  margin-bottom: 25px;
   max-width: 750px;
 `;
+
 const HeroText = styled.p`
   font-size: 1.5em;
   max-width: 660px;
+  margin-bottom: 25px;
+`;
+
+const Email = styled.a`
+  font-size: 1.5em;
 `;
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <HeroHeadline dangerouslySetInnerHTML={{ __html: data.heroheadline }} />
-            <HeroText dangerouslySetInnerHTML={{ __html: data.herosubline }} />
-            <a href="mailto:hi@dominickolbe.dk">hi@dominickolbe.dk</a>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <HeroHeadline dangerouslySetInnerHTML={{ __html: content.heroheadline }} />
+        <HeroText dangerouslySetInnerHTML={{ __html: content.herosubline }} />
+        <Email href={content.email}>{content.email}</Email>
+      </Container>
     );
   }
 }
