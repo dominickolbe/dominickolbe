189b41e68cea07612bf38b74fe380774b8292531
Dominic Kolbe <kolbedominic@gmail.com>
2019-01-12 21:36:38 +0100

    feat: update template and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import data from './data.json';
 
 const HeroHeadline = styled.h1`
   font-size: 3em;
   font-weight: 700;
+  letter-spacing: .03em;
+  line-height: 1.2;
   max-width: 750px;
 `;
 const HeroText = styled.p`
   font-size: 1.5em;
-  font-weight: 500;
-  max-width: 750px;
+  max-width: 660px;
 `;
 
 class App extends Component {
@@ -18,12 +20,8 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col">
-            <HeroHeadline>
-              Hey there, my name is Dominic, developer, I love music and video games.
-            </HeroHeadline>
-            <HeroText>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </HeroText>
+            <HeroHeadline dangerouslySetInnerHTML={{ __html: data.heroheadline }} />
+            <HeroText dangerouslySetInnerHTML={{ __html: data.herosubline }} />
             <a href="mailto:hi@dominickolbe.dk">hi@dominickolbe.dk</a>
           </div>
         </div>
