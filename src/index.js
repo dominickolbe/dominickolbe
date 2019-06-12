500902bbb19869b264d352184264d07ca44aef21
Dominic Kolbe <kolbedominic@gmail.com>
2019-06-12 12:07:24 +0100

    Add darkmode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./App";
+import { darkmode } from "./utils";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+darkmode();
---
+++ src/utils.js
@@ -0,0 +1,6 @@
+export const darkmode = () => {
+  const now = new Date().getHours();
+  if (now > 18 || now < 8) {
+    document.body.classList.add("darkmode");
+  }
+};
